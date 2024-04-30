7be2117465189f19d57a5aef4d5c07eac96d46fa
jpablo903 <jpablo_903@hotmail.com>
2024-04-30 17:19:53 -0300

    Validaciones de formulario

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -9,5 +9,45 @@ function scrollFunction() {
 }
 
 function volverArriba() {
-  document.documentElement.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 }
+
+function validarFormulario() {
+  var nombre = document.getElementById('nombre').value;
+  var email = document.getElementById('email').value;
+  var telefono = document.getElementById('telefono').value;
+
+  if (nombre == "" || email == "" || telefono == "") {
+    alert("Todos los campos son obligatorios.");
+    return false;
+  }
+
+  var Nombre = /^[a-zA-Z\s]*$/;
+  if (!Nombre.test(nombre)) {
+    alert("El nombre solo puede contener caracteres alfabéticos.");
+    return false;
+  }
+
+  var Email = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  if (!Email.test(email)) {
+    alert("Por favor, ingresa un correo electrónico válido.");
+    return false;
+  }
+
+  var Telefono = /^\d{10}$/;
+  if (!Telefono.test(telefono)) {
+    alert("El teléfono incorrecto, debe contener solo 10 números.");
+    return false;
+  }
+
+  localStorage.setItem('enviado', 'true');
+
+  return true;
+}
+
+window.onload = function() {
+  if (localStorage.getItem('enviado') === 'true') {
+      document.getElementById('formularioDeContacto').reset();
+      localStorage.removeItem('enviado');
+  }
+};
